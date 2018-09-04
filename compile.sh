b4cb24180fbaf5da1e546260ba8e525c6c451a9f
Eric Earl <earl@ohsu.edu>
2018-09-04 07:01:56 -0700

    More compile.sh updates to make it work right.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
-set -e
-# compiles all project matlab code using the matlab compiler tool
+# compiles all 
 
 function usage() {
    echo "usage: `basename $0` <Matlab Compiler>"
@@ -22,18 +21,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 CODE="${DIR}/matlab_code"
 BIN="${DIR}/bin"
 HCP_MATLAB_PATH="${CODE}/hcp_matlab"
-FRAMEWISE_DISPLACEMENT_PATH="${CODE}/framewise_displacement"
-SCRIPTS_PATH="${CODE}/scripts"
+FRAMEWISE_DISPLACEMENT="${CODE}/framewise_displacement"
+ADDITIONAL_SCRIPTS="${CODE}/scripts"
 
 rm -f ${DIR}/bin/*
 
-${MCC_FILE} -v -m -R -singleCompThread -R -nodisplay -o filtered_movement_regressors    "${CODE}"/filtered_movement_regressors.m    -a ${HCP_MATLAB_PATH} -a ${SCRIPTS_PATH}
+fmr="${MCC_FILE} -v -m -R -singleCompThread -R -nodisplay -o filtered_movement_regressors ${CODE}/filtered_movement_regressors.m -a ${HCP_MATLAB_PATH} -a ${ADDITIONAL_SCRIPTS}"
+dsp="${MCC_FILE} -v -m -R -singleCompThread -R -nodisplay -o dcan_signal_processsing      ${CODE}/dcan_signal_processing.m       -a ${HCP_MATLAB_PATH} -a ${ADDITIONAL_SCRIPTS} -a ${FRAMEWISE_DISPLACEMENT}"
+av2="${MCC_FILE} -v -m -R -singleCompThread -R -nodisplay -o analyses_v2                  ${CODE}/analyses_v2.m                  -a ${HCP_MATLAB_PATH} -a ${ADDITIONAL_SCRIPTS} -a ${FRAMEWISE_DISPLACEMENT}"
 
-${MCC_FILE} -v -m -R -singleCompThread -R -nodisplay -o dcan_signal_processsing         "${CODE}"/DCAN_Signal_Processing.m          -a ${HCP_MATLAB_PATH} -a ${SCRIPTS_PATH} -a ${FRAMEWISE_DISPLACEMENT_PATH}
+echo $fmr
+eval $fmr
+echo $dsp
+eval $dsp
+echo $av2
+eval $av2
 
-${MCC_FILE} -v -m -R -singleCompThread -R -nodisplay -o analyses_v2                     "${CODE}"/analyses_v2.m                     -a ${HCP_MATLAB_PATH} -a ${SCRIPTS_PATH} -a ${FRAMEWISE_DISPLACEMENT_PATH}
-
-mv filtered_movement_regressors dcan_signal_processsing analyses_v2 run_filtered_movement_regressors.sh run_dcan_signal_processsing.sh run_analyses_v2.sh "$BIN"/
+mv filtered_movement_regressors dcan_signal_processsing analyses_v2 run_*.sh ${BIN}/
 
 #add MCR_CACHE_ROOT to all run scripts for Exahead processing
 sed -i '/exe_dir=`dirname "$0"`/a if [ ! -d $TMPDIR/$USER ]; then\n    mkdir $TMPDIR/$USER\nfi\nexport MCR_CACHE_ROOT=$TMPDIR/$USER' ${BIN}/run_*.sh
